5414ca6a3c995990423765f59f487cab40a0b652
wondergo2017 <wondergo2017@gmail.com>
2023-09-08 17:29:37 +0000

    setup

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from setuptools import setup, find_packages
 
-__version__ = '0.0.1'
-URL = None
+with open("readme.md", 'r') as fh:
+    long_description = fh.read()
+    
+__version__ = '0.0.2'
+URL = "https://github.com/wondergo2017/libwon/tree/main"
 install_requires = [
     "matplotlib",
     "pandas",
@@ -11,8 +14,11 @@ install_requires = [
 setup(
     name='libwon',
     version=__version__,
-    description='libwon',
     author='wondergo',
+    author_email='wondergo2017@gmail.com',
+    description='A simple lib to automatically manage the parallelled experiments',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url=URL,
     python_requires='>=3.6',
     install_requires=install_requires,
